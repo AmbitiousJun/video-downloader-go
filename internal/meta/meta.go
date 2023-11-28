7a82a1f55e670edea425d12fb9471c4c9a2d4870
Ambitious <1535820771@qq.com>
2023-11-28 13:01:19 +0800

    refactor: rename root entity dir to meta

--- internal/meta/meta.go
+++ internal/meta/meta.go
@@ -3,20 +3,20 @@ package entity
 import "video-downloader-go/internal/util/myhttp"
 
 // 视频文件元数据
-type VideoMeta struct {
+type Video struct {
 	Name string // 视频名称
 	Url  string // 视频地址
 }
 
-type DownloadMeta struct {
+type Download struct {
 	Link      string            // 视频下载地址
 	FileName  string            // 视频名称
 	OriginUrl string            // 源视频地址
 	HeaderMap map[string]string // 请求头
 }
 
-func NewDownloadMeta(link, fileName, originUrl string) *DownloadMeta {
-	dm := DownloadMeta{Link: link, FileName: fileName, OriginUrl: originUrl}
+func NewDownloadMeta(link, fileName, originUrl string) *Download {
+	dm := Download{Link: link, FileName: fileName, OriginUrl: originUrl}
 	m := myhttp.GenDefaultHeaderMapByUrl(nil, link)
 	dm.HeaderMap = m
 	return &dm
