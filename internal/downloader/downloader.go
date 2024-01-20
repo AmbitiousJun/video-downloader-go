dfc7abed865de27a953df03566d4aa82cbca0fa4
ambitious <1535820771@qq.com>
2024-01-20 17:44:48 +0800

    feat: optimize download progress output

--- internal/downloader/downloader.go
+++ internal/downloader/downloader.go
@@ -5,7 +5,6 @@ package downloader
 import (
 	"fmt"
 	"log"
-	"os"
 	"path/filepath"
 	"strings"
 	"time"
@@ -16,8 +15,6 @@ import (
 	"video-downloader-go/internal/meta"
 	"video-downloader-go/internal/util/myfile"
 	"video-downloader-go/internal/util/mylog"
-
-	"golang.org/x/term"
 )
 
 // 错误信息
@@ -90,54 +87,6 @@ func handleTask(dmt *meta.Download, completeOne CompleteOne, dlErrorHandler DlEr
 		mylog.Errorf("下载失败：%v，重新加入下载列表", err)
 		offerBack(dmt)
 	})
-}
-
-// printDownloadProgress 负责将下载进度日志输出到控制台上
-func printDownloadProgress(fileName string, p *coredl.Progress) {
-	percent := float64(p.Current) / float64(p.Total)
-	curDlMb := float64(p.CurrentBytes) / 1024 / 1024
-	totDlMb := float64(p.TotalBytes) / 1024 / 1024
-
-	// 获取控制台中一行可以显示多少个字符，用于显示进度条
-	fd := int(os.Stdout.Fd())
-	width, _, err := term.GetSize(fd)
-
-	var totalBlocks, finishBlocks int
-	if err == nil {
-		// 28 是日志输出前缀长度：'2023/12/17 01:15:15 SUCCESS '
-		// 2 是输出进度条的时候的左右括号
-		totalBlocks = width - 28 - 2
-		finishBlocks = int(float64(totalBlocks) * percent)
-		if p.Current == p.Total-1 {
-			// 剩最后一个分片时，进度条拉满
-			finishBlocks = totalBlocks
-		}
-	}
-
-	if err == nil {
-		// 清空最后 9 行日志
-		mylog.Success("\033[9F\033[J\r")
-		mylog.Success("==== 下载进度 ⬇️")
-
-		// 输出进度条
-		mylog.Successf("[%v%v]", strings.Repeat("*", finishBlocks), strings.Repeat("-", totalBlocks-finishBlocks))
-
-		// 控制文件名的长度不超过 1 行
-		maxLen := int(float64(width) * 0.6)
-		if len(fileName) > maxLen {
-			fileName = fileName[:maxLen] + "..."
-		}
-	} else {
-		mylog.Success("==== 下载进度 ⬇️")
-		mylog.Success("")
-	}
-
-	mylog.Successf("文件名：%v", fileName)
-	mylog.Successf("分片进度：%v/%v(%.2f%%)", p.Current, p.Total, percent*100)
-	mylog.Successf("文件大小：%.2f/%.2f (MB)", curDlMb, totDlMb)
-	mylog.Successf("任务进度：%v/%v", p.CurrentTask, p.TotalTasks)
-	mylog.Successf("下载速率：%s", config.RateLimitBucket().CurrentRateStr)
-	mylog.Success("==== 下载进度 ⬆️")
 }
 
 // initCoreDownloader 根据全局配置初始化下载器对象
