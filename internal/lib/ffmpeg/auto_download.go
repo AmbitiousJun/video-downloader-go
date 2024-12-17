3c73886f79f0e32212417b9db0a598822ce948fa
ambitious <1535820771@qq.com>
2024-12-17 09:47:29 +0800

    feat: add ffmpeg auto download support

--- internal/lib/ffmpeg/auto_download.go
+++ internal/lib/ffmpeg/auto_download.go
@@ -1,4 +1,4 @@
-package ytdlp
+package ffmpeg
 
 import (
 	"fmt"
@@ -14,27 +14,26 @@ import (
 
 const (
 
-	// ReleasePage yt-dlp 发布页地址
-	ReleasePage = "https://github.com/yt-dlp/yt-dlp/releases/latest/download"
+	// ReleasePage ffmpeg 发布页地址
+	ReleasePage = "https://github.com/AmbitiousJun/ffmpeg-release/releases/latest/download"
 )
 
 // arch2ExecNameMap 根据系统的芯片架构, 映射到对应的二进制文件
 var arch2ExecNameMap = map[string]string{
-	"darwin/amd64":  "yt-dlp_macos",
-	"darwin/arm64":  "yt-dlp_macos",
-	"windows/386":   "yt-dlp_x86.exe",
-	"windows/amd64": "yt-dlp.exe",
-	"windows/arm":   "yt-dlp.exe",
-	"windows/arm64": "yt-dlp.exe",
-	"linux/386":     "yt-dlp_linux",
-	"linux/amd64":   "yt-dlp_linux",
-	"linux/arm":     "yt-dlp_linux_armv7l",
-	"linux/arm64":   "yt-dlp_linux_aarch64",
+	"darwin/amd64":  "ffmpeg_macos",
+	"darwin/arm64":  "ffmpeg_macos",
+	"windows/386":   "ffmpeg.exe",
+	"windows/amd64": "ffmpeg.exe",
+	"windows/arm":   "ffmpeg.exe",
+	"windows/arm64": "ffmpeg.exe",
+	"linux/386":     "ffmpeg_linux_386",
+	"linux/amd64":   "ffmpeg_linux_amd64",
+	"linux/arm":     "ffmpeg_linux_arm",
+	"linux/arm64":   "ffmpeg_linux_arm64",
 }
 
 var (
-	parentPath = filepath.Join(constant.Dir_DataRoot, "yt-dlp") // 二进制文件存放根路径
-	execOk     = false                                          // 标记二进制文件是否处于就绪状态
+	parentPath = filepath.Join(constant.Dir_DataRoot, "ffmpeg") // 二进制文件存放根路径
 	execPath   string                                           // 根据当前系统架构自动生成一个二进制文件地址
 )
 
@@ -42,7 +41,7 @@ func ExecPath() string {
 	return execPath
 }
 
-// AutoDownloadExec 自动根据系统架构下载对应版本的 yt-dlp 到数据目录下
+// AutoDownloadExec 自动根据系统架构下载对应版本的 ffmpeg 到数据目录下
 //
 // 下载失败只会进行日志输出, 不会影响到程序运行
 func AutoDownloadExec() error {
@@ -52,7 +51,7 @@ func AutoDownloadExec() error {
 	// 生成二进制文件地址
 	execName, ok := arch2ExecNameMap[fmt.Sprintf("%s/%s", gos, garch)]
 	if !ok {
-		return fmt.Errorf("不支持的芯片架构: %s/%s, yt-dlp 相关功能失效", gos, garch)
+		return fmt.Errorf("不支持的芯片架构: %s/%s, ffmpeg 相关功能失效", gos, garch)
 	}
 	execPath = fmt.Sprintf("%s/%s", parentPath, execName)
 
@@ -62,18 +61,17 @@ func AutoDownloadExec() error {
 		if stat.IsDir() {
 			return fmt.Errorf("二进制文件路径被目录占用: %s, 请手动处理后尝试重启服务", execPath)
 		}
-		execOk = true
-		fmt.Println(color.ToGreen("yt-dlp 环境检测通过 ✓"))
+		fmt.Println(color.ToGreen("ffmpeg 环境检测通过 ✓"))
 		return nil
 	}
 
-	fmt.Println(color.ToBlue("检测不到 yt-dlp 环境, 即将开始自动下载"))
+	fmt.Println(color.ToBlue("检测不到 ffmpeg 环境, 即将开始自动下载"))
 
 	if err = os.MkdirAll(parentPath, os.ModePerm); err != nil {
 		return fmt.Errorf("数据目录异常: %s, err: %v", parentPath, err)
 	}
 
-	fmt.Printf(color.ToBlue("yt-dlp 下载发布页: %s\n"), ReleasePage)
+	fmt.Printf(color.ToBlue("ffmpeg 下载发布页: %s\n"), ReleasePage)
 
 	_, resp, err := https.Request(http.MethodGet, ReleasePage+"/"+execName, nil, nil, true)
 	if err != nil {
@@ -93,7 +91,6 @@ func AutoDownloadExec() error {
 	io.Copy(execFile, resp.Body)
 
 	// 标记就绪状态
-	execOk = true
-	fmt.Printf(color.ToGreen("yt-dlp 自动下载成功 ✓, 路径: %s\n"), execPath)
+	fmt.Printf(color.ToGreen("ffmpeg 自动下载成功 ✓, 路径: %s\n"), execPath)
 	return nil
 }
